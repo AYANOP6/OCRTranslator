5ce97898f9de1359b271a55d3845061159cca39b
Rahul <rhlydv961@gmail.com>
2021-12-22 12:06:23 +0530

    22decUpdates

--- app/src/main/java/com/hmmrahul/ocrtranslator/MainActivity.java
+++ app/src/main/java/com/hmmrahul/ocrtranslator/MainActivity.java
@@ -4,50 +4,30 @@ import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
-import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 import com.hmmrahul.ocrtranslator.databinding.ActivityMainBinding;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
-
-import java.io.IOException;
 
 import io.ak1.OnBubbleClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
-    Bitmap bitmap;
     private static final int REQUEST_CAMERA_CODE = 100;
 
-    String[] fromLanguages = {"From", "English", "Bengali", "Hindi"};
-    String[] ToLanguages = {"To", "English", "Bengali", "Hindi"};
+
+    Fragment activefragment;
+    final Fragment ocrFragment = new ScanFragment();
+    final Fragment manualTranslatorFragment = new ManualTranslatorFragment();
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
 
     @Override
@@ -56,83 +36,39 @@ public class MainActivity extends AppCompatActivity {
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
 
-
-
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_CODE);
         }
 
-        mainBinding.bubbleTabBar.setSelectedWithId(R.id.ocrTab,false);
+        fragmentManager.beginTransaction().add(mainBinding.mainFrameLayout.getId(), ocrFragment).commit();
+        activefragment = ocrFragment;
+        fragmentManager.beginTransaction().add(mainBinding.mainFrameLayout.getId(), manualTranslatorFragment).hide(manualTranslatorFragment).commit();
+        mainBinding.bubbleTabBar.setSelectedWithId(R.id.ocrTab, false);
+
         mainBinding.bubbleTabBar.addBubbleListener(new OnBubbleClickListener() {
             @Override
             public void onBubbleClick(int i) {
-                String te = String.valueOf(i);
-                Log.d("He",te);
                 switch (i) {
                     case R.id.ocrTab:
+                        fragmentManager.beginTransaction().hide(activefragment).show(ocrFragment).commit();
+                        activefragment = ocrFragment;
                         break;
                     case R.id.translatorTab:
-                        startActivity(new Intent(getApplicationContext(),TranslatorActivity.class));
-                        overridePendingTransition(0,0);
+                        fragmentManager.beginTransaction().hide(activefragment).show(manualTranslatorFragment).commit();
+                        activefragment = manualTranslatorFragment;
                         break;
                 }
             }
         });
-        mainBinding.buttonCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //It will Launch the Crop image Activity
-                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this);
-            }
-        });
 
-        mainBinding.copyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String scannedText = mainBinding.textData.getText().toString();
-                copyToClipBoard(scannedText);
-                translateText(scannedText);
-            }
-        });
-    }
-
-
-        @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri();
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
-                    getTextFromImage(bitmap);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private void getTextFromImage(Bitmap bitmap) {
-        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
-        if (!recognizer.isOperational()) {
-            Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
-        } else {
-            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < textBlockSparseArray.size(); i++) {
-                TextBlock textBlock = textBlockSparseArray.valueAt(i);
-                stringBuilder.append(textBlock.getValue());
-                stringBuilder.append("\n");
-            }
-            mainBinding.animationView.setVisibility(View.GONE);
-            mainBinding.textData.setText(stringBuilder.toString());
-            mainBinding.buttonCapture.setText("Retake");
-            mainBinding.copyBtn.setVisibility(View.VISIBLE);
-        }
+//        mainBinding.copyBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String scannedText = mainBinding.textData.getText().toString();
+//                copyToClipBoard(scannedText);
+//                translateText(scannedText);
+//            }
+//        });
     }
 
     private void copyToClipBoard(String text) {
@@ -142,55 +78,4 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "Copied to Clipboard", Toast.LENGTH_SHORT).show();
     }
 
-    private void translateText(String input) {
-        FirebaseTranslatorOptions options = new FirebaseTranslatorOptions.Builder()
-                .setSourceLanguage(FirebaseTranslateLanguage.EN)
-                .setTargetLanguage(FirebaseTranslateLanguage.GU)
-                .build();
-
-        FirebaseTranslator translator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
-        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder().build();
-        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                translator.translate(input)
-                        .addOnSuccessListener(new OnSuccessListener<String>() {
-                            @Override
-                            public void onSuccess(String s) {
-//                                 mainBinding.textData.setText(s);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Fail to Translate" + e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "Fail to Download Language Model" + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    //    String[] fromLanguages = {"From","English","Bengali","Hindi"};
-    public int getLanguageCode(String language) {
-        int languageCode = 0;
-        switch (language) {
-            case "English":
-                languageCode = FirebaseTranslateLanguage.EN;
-                break;
-            case "Bengali":
-                languageCode = FirebaseTranslateLanguage.BN;
-                break;
-            case "Hindi":
-                languageCode = FirebaseTranslateLanguage.HI;
-                break;
-            default:
-                languageCode = 0;
-        }
-        return languageCode;
-    }
 }
